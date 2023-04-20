ccc9c3ba8b97428e351848456bdcba51483f5a2d
Matias Seitour <matias.seitour01@gmail.com>
2023-04-20 14:20:50 -0300

    feat(logo) change logo image

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,12 +5,8 @@ export default function Logo() {
         
         <div className="w-full h-[400px] flex justify-center items-center">
             <figure className="relative w-full h-full flex justify-center items-center">
-                <Image className="absolute object-cover" priority={true} width={0} height={0} style={{ width: '100%', height: '100%', objectFit: "cover" }} sizes="100vw" src={"/background.png"} alt="backgroundHome" />
-                <div className="title-container relative w-[200px] h-[200px] bg-white z-1 rounded-full overflow-hidden animate-logo shadow-xl">
-                    <h2 className="font-alexBrush text-3xl text-blue-700 font-bold flex items-center justify-center w-full h-full text-center">Almacén <br />Buena vida</h2>
-                    <Image className="absolute z-1 top-[50%] animate-trigoLogo item-trigo-1 object-cover"  priority={true} width={60} height={60} src={"/trigo.png"} alt="trigo" />
-                    <Image className="absolute z-1 top-[80%] animate-trigoLogo2 item-trigo-2 object-cover" priority={true} width={60} height={60} src={"/trigo.png"} alt="trigo" />
-                </div>
+                <Image className="object-cover absolute z-[10]" priority={true} width={340} height={340} sizes="100vw" src={"/logo.png"} alt="logo" />
+                <Image className="absolute object-cover z-[1] grayscale" priority={true} width={0} height={0} style={{ width: '100%', height: '100%', objectFit: "cover" }} sizes="100vw" src={"/background.png"} alt="backgroundHome" />
             </figure>
         </div>
     )
