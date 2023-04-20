ca20d4d84b12be2da44bdf2fb7a9fba901531df9
Matias Seitour <matias.seitour01@gmail.com>
2023-04-20 14:18:20 -0300

    feat(components) add layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import Nav from '@/components/Nav'
+import ContactSection from '@/components/ContactSection'
+import Head from 'next/head'
+
+export default function Layout({children}){
+    return(
+        <>
+            <Head>
+                <title>Almacen Buenavida</title>
+                <meta name='description' content='Pagina web de almacen buenavida'></meta>
+            </Head>
+            <Nav />
+            {children}
+            <ContactSection />
+        </>
+    )
+}
