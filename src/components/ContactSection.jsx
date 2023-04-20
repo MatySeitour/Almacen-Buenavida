f13020550fdcd91d1b9e52d297d25344e46c22fb
Matias Seitour <matias.seitour01@gmail.com>
2023-04-20 14:27:36 -0300

    feat(footer) change footer

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -3,44 +3,57 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function ContactSection() {
     return (
-        <footer className="w-full h-[auto] bg-green-700 p-4">
-            <div className="w-full h-full flex justify-around items-center flex-col">
-                <div className="mb-10">
-                    <h3 className="text-white text-2xl font-semibold text-center">¿Tenes alguna Consulta?</h3>
-                    <div>
-                        <p className="text-white text-2xl font-semibold text-center">¡¡Contactame!!</p>
+        <footer className="w-full h-[auto] bg-gray-400">
+            <div className="w-full h-full flex justify-around items-center flex-col p-4">
+                <div className="w-full h-auto flex flex-row p-4">
+                    <div className="w-full h-auto flex flex-col">
+                        <p className="text-base text-white mb-4 font-bold">EXPLORAR</p>
+                        <ul className="w-full h-auto flex flex-col items-start justify-center">
+                            <li className="text-white text-sm mb-4 font-medium">INICIO</li>
+                            <li className="text-white text-sm mb-4 font-medium">OFERTAS</li>
+                            <li className="text-white text-sm mb-4 font-medium">PRODUCTOS DESTACADOS</li>
+                            <li className="text-white text-sm mb-4 font-medium">CATEGORIAS</li>
+                        </ul>
+                    </div>
+                    <div className="w-full h-auto flex flex-col ">
+                        <p className="text-base text-white mb-4 font-bold text-right">MÁS INFORMACIÓN</p>
+                        <ul className="w-full h-auto flex flex-col items-end justify-center">
+                            <li className="text-white text-sm mb-4 font-medium">COMO COMPRAR</li>
+                            <li className="text-white text-sm mb-4 font-medium">OFERTAS</li>
+                            <li className="text-white text-sm mb-4 font-medium">SOBRE NOSOTROS</li>
+                            <li className="text-white text-sm mb-4 font-medium text-right">CONTACTO</li>
+                        </ul>
                     </div>
                 </div>
-                <div className="w-full h-auto flex flex-row justify-evenly items-center mb-8">
-                    <div className="h-auto flex flex-1 flex-col justify-center items-center mb-6">
-                        <div className="w-[50px] h-[50px] bg-white p-2 rounded-full flex items-center justify-center mb-2">
-                            <FontAwesomeIcon className="text-4xl text-green-500" icon={faWhatsapp} />
+                <div className="w-full h-auto p-4">
+                    <p className="text-base text-white mb-4 font-bold">CONTACTÁNOS</p>
+                    <div className="w-full h-auto flex">
+                        <div className="w-auto h-auto bg-gray-700 p-2 mr-2 rounded-full">
+                            <FontAwesomeIcon className="w-[26px] h-[26px] text-white" icon={faWhatsapp} />
                         </div>
-                        <p className="text-white text-lg">11XXXX-XXXX</p>
-                    </div>
-                    <div className="h-auto flex flex-1 flex-col justify-center items-center mb-6">
-                        <div className="w-[50px] h-[50px] bg-white p-2 rounded-full flex items-center justify-center mb-2">
-                            <FontAwesomeIcon className="text-4xl text-green-500" icon={faInstagram} />
+                        <div className="w-auto h-auto bg-gray-700 p-2 rounded-full">
+                            <FontAwesomeIcon className="w-[26px] h-[26px] text-white" icon={faInstagram} />
                         </div>
-                        <p className="text-white text-lg">@almacen.buenavida</p>
+                        <div></div>
                     </div>
                 </div>
-                <ul className="w-full h-auto flex flex-col items-center justify-center">
-                    <li className="text-white text-lg mb-4 font-semibold">INICIO</li>
-                    <li className="text-white text-lg mb-4 font-semibold">OFERTAS</li>
-                    <li className="text-white text-lg mb-4 font-semibold">SOBRE NOSOTROS</li>
-                    <li className="text-white text-lg mb-4 font-semibold">PRODUCTOS DESTACADOS</li>
-                    <li className="text-white text-lg mb-4 font-semibold">CATEGORIAS</li>
-                </ul>
-                <div className="w-full h-auto p-2 bg-green-400">
+                <div className="w-full h-auto p-4">
+                    <p className="text-base text-white mb-4 font-bold">UBICACIÓN</p>
+                    <p className="text-white font-semibold">
+                        San Martín 1142 entre Pellegrini y Sarmiento. Merlo.
+                    </p>
+                </div>
+            </div>
+
+                <div className="w-full h-auto bg-gray-700 p-2">
                     <p className="text-white flex justify-center items-center text-center">
                         Copyright Almacén Buenavida - 2023.
                         <br />
                         Todos los derechos reservados.
                         <br />
-                        Defensa de los consumidores. </p>
+                        Defensa de los consumidores. 
+                    </p>
                 </div>
-            </div>
         </footer>
     )
 }
