431a26b05e8923adfd0bbeb8119514dc5b5d2060
Matias Seitour <matias.seitour01@gmail.com>
2023-04-24 14:55:14 -0300

    feat(utils/api) add slug property in categories

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,6 @@ import _products from './products.json'
 import _categories from './categories.json'
 import _productsOffers from "./_products_offers.json"
 
-console.log(_productsOffers);
 
 export default {
     getCategories: () => _categories.categories,
