03ebf8b57664d368dede595b3f2584e373afbf4a
Matias Seitour <matias.seitour01@gmail.com>
2023-04-25 23:07:41 -0300

    feat(subCategories) add subCategories component

---
+++ src/components/SubCategories.jsx
@@ -0,0 +1,13 @@
+export default function SubCategories({subCategoriesState, categories, handleSubLevelSelect }){
+    console.log(categories)
+
+    return(
+        <div className="w-[100%] flex absolute left-0 top-12 z-10 overflow-y-scroll">
+            <ul className={subCategoriesState ? "bg-green-500 rounded-md shadow-md w-full opacity-0 transition-all overflow-scroll max-h-0" : "bg-green-700 rounded-b-md shadow-md w-full opacity-100 transition-all overflow-scroll max-h-[150px] pb-2"}>
+                {categories.map((subCategory) => (
+                    <li onClick={() => handleSubLevelSelect(subCategory.id)} key={subCategory.id} className="text-white h-[auto] p-1">{subCategory.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
