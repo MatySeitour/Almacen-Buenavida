8b91ffca14e18f07df8ffe18d567ab902cd345fe
Matias Seitour <matias.seitour01@gmail.com>
2023-04-29 12:53:54 -0300

    feat(cartHome) change styles to cartHome component

--- src/components/CartHome.jsx
+++ src/components/CartHome.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
+import { faAngleLeft, faArrowLeft } from "@fortawesome/free-solid-svg-icons"
 import { useCart } from "@/context/CartContext"
 import CartItem from "./CartItem";
 
@@ -8,19 +8,20 @@ export default function CartHome({ cartShow }) {
 
     return (
         <div className={cartShow ? "fixed w-full h-full top-0 left-0 right-0 z-[200] bg-[#000a]" : ""}>
-            <div className={cartShow ? "fixed w-[340px] h-full translate-x-0 transition-all bg-white top-0 right-0 z-[10000] overflow-y-scroll pt-[70px]" : "fixed w-[340px] h-full translate-x-[500px] transition-all bg-white top-0 right-0 z-[10000] overflow-y-scroll pt-[70px]"}>
-                <div className="fixed top-0 w-[340px] h-auto bg-green-300 flex justify-between items-center z-[10000000] pr-2">
-                    <div onClick={() => setCartShow(state => !state)} className="w-14 h-14 flex justify-center items-center">
-                        <FontAwesomeIcon className="w-8 h-8 text-white text-lg" icon={faArrowLeft} />
+            <div className={cartShow ? "fixed w-full h-full translate-x-0 transition-all bg-gray-100 top-0 right-0 z-[10000] pt-[70px] duration-200" : "fixed w-full h-full translate-x-[500px] transition-all duration-200 bg-white top-0 right-0 z-[10000] overflow-y-scroll pt-[70px]"}>
+                <div className="fixed top-0 w-full h-auto min-h-[60px] flex justify-between items-center z-[10000000] px-4">
+                    <div onClick={() => setCartShow(state => !state)} className="w-[40px] h-[40px] shadow-md rounded-md flex justify-center items-center bg-white">
+                        <FontAwesomeIcon className="w-7 h-7 text-green-500 text-lg" icon={faAngleLeft} />
                     </div>
-                    <div>
-                        <h4 className="text-white text-lg tracking-wide font-medium">CARRITO DE COMPRAS</h4>
+                    <div className="w-[70%] p-2 text-center">
+                        <h4 className="text-green-500 text-base tracking-wide font-medium">CARRITO DE COMPRAS</h4>
                     </div>
+                    <div></div>
                 </div>
 
                 {cartItems.length !== 0 ?
-                    <div className="w-full h-auto flex flex-col justify-between items-center">
-                        <ul className="w-full h-full mb-10">
+                    <div className="w-full h-full flex flex-col justify-between items-center">
+                        <ul className="w-full h-full max-h-[500px] overflow-y-scroll">
                             {cartItems.map((item) => (
                                 <CartItem
                                     key={item.id}
@@ -34,14 +35,13 @@ export default function CartHome({ cartShow }) {
                             ))}
                         </ul>
 
-                        <div className="w-full h-auto max-h-[400px] py-4 px-2 border-t border-green-500">
-                            <div className="w-full h-auto flex justify-around items-start mb-4">
-                                <p className="text-2xl text-green-500 font-bold">Total:</p>
-                                <p className="text-2xl text-green-500 font-bold">{`$${totalCart}`}</p>
+                        <div className="w-full h-auto flex shadow-default py-4 px-2">
+                            <div className="w-full h-auto flex flex-col justify-around items-start">
+                                <p className="text-xl text-black font-bold">Total</p>
+                                <p className="text-lg text-green-500 font-medium">{`$${totalCart}`}</p>
                             </div>
-                            <div className="w-full h-auto flex flex-col justify-around items-center">
-                                <button className="mb-2 p-2 w-[90%] bg-green-500 rounded-md text-white tracking-wide font-semibold" type="text">COMPRAR</button>
-                                <p className="text-green-500 mt-2">Seguir comprando</p>
+                            <div className="w-full h-auto flex flex-col justify-center items-center">
+                                <button className="mb-2 p-2 w-[90%] bg-green-500 rounded-md text-white tracking-wide font-medium text-base" type="text">REVISAR COMPRA</button>
                             </div>
                         </div>
                     </div>
