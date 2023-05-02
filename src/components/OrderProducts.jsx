ed434731a5df19fde3559691429d7c81752a6982
Matias Seitour <matias.seitour01@gmail.com>
2023-05-02 10:39:09 -0300

    feat(orderProducts) add selectbox order

--- src/components/OrderProducts.jsx
+++ src/components/OrderProducts.jsx
@@ -1,12 +1,47 @@
-export default function OrderProducts({orderFilterState}){
+export default function OrderProducts({orderFilterState, setOrderFilterState}){
+
+    const order = [
+        {
+            id: 1,
+            value: "Mayor precio"
+        },
+        {
+            id: 2,
+            value: "Menor precio"
+        },
+        {
+            id: 3,
+            value: "A - Z"
+        },
+        {
+            id: 4,
+            value: "Z - A"
+        }
+    ]
+
+    const handleNewListOrder = (order) => {
+        setOrderFilterState({
+            active: false,
+            value: order.value
+        })
+
+        console.log(orderFilterState.active)
+    }
+
     return(
-        <div className="w-[100%] flex absolute right-0 top-14 z-10 overflow-y-scroll">
-            <div className={orderFilterState ? "bg-green-500 rounded-md shadow-md w-full opacity-0 transition-all overflow-scroll max-h-0" : "bg-green-500 rounded-md shadow-md w-full opacity-100 transition-all overflow-scroll max-h-[150px] pb-2"}>
-                <p className="text-white h-[30px] p-2">de mayor a menor</p>
-                <p className="text-white h-[30px] p-2">de menor a mayor</p>
-                <p className="text-white h-[30px] p-2">A - Z</p>
-                <p className="text-white h-[30px] p-2">Z - A</p>
-            </div>
+        <div className="w-auto min-w-[130px] flex absolute right-0 top-[100%] translate-y-[4px] z-10 overflow-y-scroll justify-center items-center">
+            <ul className={!orderFilterState.active ? "bg-white rounded-md shadow-md w-full opacity-0 overflow-scroll max-h-0" : "bg-white rounded-md shadow-md w-full opacity-100 transition-all overflow-scroll max-h-[150px] py-2"}>
+                {order.map((option) => (
+                    <li 
+                        onClick={() => {handleNewListOrder(option)}}
+                        key={option.id} className={`text-black ${orderFilterState.value == option.value && `text-green-500 option-order__active`} h-[30px] relative text-center p-1 ${option.id != 4 && `border-b border-gray-300`}`}>
+                        {option.value}
+                    </li>
+                ))}
+                {/* <p className="text-black h-[30px] text-center p-1 border-b border-gray-300">Menor precio</p>
+                <p className="text-black h-[30px] text-center p-1 border-b border-gray-300">A - Z</p> */}
+                {/* <p className="text-black h-[30px] text-center p-1">Z - A</p> */}
+            </ul>
         </div>
     )
 }
