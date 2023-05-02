5b3ee9cba0ab90afe5b5c218f78218ce0246ca23
Matias Seitour <matias.seitour01@gmail.com>
2023-05-02 17:40:12 -0300

    feat(filter) add new styles to filter option

--- src/components/SubCategories.jsx
+++ src/components/SubCategories.jsx
@@ -1,12 +1,36 @@
-export default function SubCategories({subCategoriesState, categories, handleSubLevelSelect }){
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faX } from "@fortawesome/free-solid-svg-icons"
+
+export default function SubCategories({subCategoriesState, categories, handleSubLevelSelect, setSubCategoriesState }){
 
     return(
-        <div className="w-[100%] flex absolute left-0 top-12 z-10 overflow-y-scroll">
-            <ul className={subCategoriesState ? "bg-green-500 rounded-md shadow-md w-full opacity-0 transition-all overflow-scroll max-h-0" : "bg-green-700 rounded-b-md shadow-md w-full opacity-100 transition-all overflow-scroll max-h-[150px] pb-2"}>
-                {categories.map((subCategory) => (
-                    <li onClick={() => handleSubLevelSelect(subCategory.id)} key={subCategory.id} className="text-white h-[auto] p-1">{subCategory.name}</li>
-                ))}
-            </ul>
+        <div className={subCategoriesState ? "fixed w-full h-full top-0 left-0 right-0 z-[200] bg-[#000a]" : ""}>
+            <aside className={subCategoriesState ? "fixed w-[340px] h-full translate-x-0 transition-all bg-white top-0 right-0 z-[10000] overflow-y-scroll" : "fixed w-[340px] h-full translate-x-[500px] transition-all bg-white top-0 right-0 z-[10000] overflow-y-scroll"}>
+                <div className="w-full h-auto flex justify-between items-center py-4 px-2 bg-green-600">
+                        <FontAwesomeIcon onClick={() => setSubCategoriesState(state => !state)} className="text-white w-[24px] h-[24px] flex justify-center items-center mr-4" icon={faX}></FontAwesomeIcon>
+                        <h3 className="text-white text-xl font-semibold tracking-wide">Filtrar por</h3>
+                </div>
+                <ul className="w-full h-auto mt-4">
+                    <li className="w-full p-2 flex flex-col justify-start items-start border-b border-gray-200">
+                        <p className="text-green-500 w-auto text-base tracking-wider font-semibold">CATEGORIAS</p>
+                        <ul>
+                            {categories.map((category) => (
+                            <li className="w-full h-[auto] flex flex-row justify-between items-center" key={category.id}>
+                                <p className="text-green-500 w-auto text-sm p-2 font-normal">{category.name}</p>
+                            </li>
+                        ))}
+                        </ul>
+                    </li>
+                    <li className="w-full p-2 flex flex-row justify-between items-center border-b border-gray-200">
+                        <p className="text-green-500 w-auto text-base tracking-wider font-semibold">OFERTAS</p>
+                    </li>
+                    <li className="w-full p-2 flex flex-row justify-between items-center border-b border-gray-200">
+                        <p className="text-green-500 w-auto text-base tracking-wider font-semibold">MARCAS</p>
+                    </li>
+                    
+
+                </ul>
+            </aside>
         </div>
     )
 }
