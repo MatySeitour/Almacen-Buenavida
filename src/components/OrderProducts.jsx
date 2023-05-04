751e755fade60555a43bc881434dc0ff8d2e760e
Matias Seitour <matias.seitour01@gmail.com>
2023-05-04 19:55:27 -0300

    feat(orderProducts) change position right to left && add max-width

--- src/components/OrderProducts.jsx
+++ src/components/OrderProducts.jsx
@@ -24,13 +24,11 @@ export default function OrderProducts({orderFilterState, setOrderFilterState}){
             active: false,
             value: order.value
         })
-
-        console.log(orderFilterState.active)
     }
 
     return(
-        <div className="w-auto min-w-[130px] flex absolute right-0 top-[100%] translate-y-[4px] z-10 overflow-y-scroll justify-center items-center">
-            <ul className={!orderFilterState.active ? "bg-white rounded-md shadow-md w-full opacity-0 overflow-scroll max-h-0" : "bg-white rounded-md shadow-md w-full opacity-100 transition-all overflow-scroll max-h-[150px] py-2"}>
+        <div className="w-auto min-w-[130px] flex absolute right-0 left-0 top-[100%] translate-y-[4px] z-10 overflow-y-scroll justify-center items-center">
+            <ul className={!orderFilterState.active ? "bg-white rounded-md shadow-md w-full opacity-0 overflow-scroll max-h-0" : "bg-white rounded-md shadow-md w-full opacity-100 transition-all overflow-scroll max-h-[150px] max-w-[150px] py-2  border border-gray-200"}>
                 {order.map((option) => (
                     <li 
                         onClick={() => {handleNewListOrder(option)}}
@@ -38,9 +36,6 @@ export default function OrderProducts({orderFilterState, setOrderFilterState}){
                         {option.value}
                     </li>
                 ))}
-                {/* <p className="text-black h-[30px] text-center p-1 border-b border-gray-300">Menor precio</p>
-                <p className="text-black h-[30px] text-center p-1 border-b border-gray-300">A - Z</p> */}
-                {/* <p className="text-black h-[30px] text-center p-1">Z - A</p> */}
             </ul>
         </div>
     )
