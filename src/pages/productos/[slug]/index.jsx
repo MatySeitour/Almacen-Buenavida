1b09331076e6f4fd9e0e422739d5d2433f7401d3
Matias Seitour <matias.seitour01@gmail.com>
2023-05-04 19:54:20 -0300

    feat(productos/slug/index) add new filter section && add some styles

--- src/pages/productos/[slug]/index.jsx
+++ src/pages/productos/[slug]/index.jsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 import { useCart } from "@/context/CartContext";
 import Layout from "@/components/Layout";
 import { useCategory } from "@/context/CategoriesContext";
-import SubCategories from "@/components/SubCategories";
+import Filter from "@/components/Filter";
 import OrderProducts from "@/components/OrderProducts";
 
 export async function getServerSideProps(context){
@@ -26,7 +26,7 @@ export async function getServerSideProps(context){
 export default function Slug({slug}){
     const {cartShow} = useCart();
 
-    const {setSubCategoriesState, subCategoriesState} = useCategory();
+    const {setFilterProductState, filterProductState} = useCategory();
 
     const [subLevelsState, setSubLevelsState] = useState(0);
     const [orderFilterState, setOrderFilterState] = useState({
@@ -37,15 +37,16 @@ export default function Slug({slug}){
     
     const categories = getCategories.categories;
     const categorySelected = categories.filter(category => category.slug == slug);
+    const [filterSelected, setFilterSelected] = useState([]);
 
     const products = getProducts.products;
     const productsByCategory = products.filter(product => product.categoryId == categorySelected[0].id);
     const productsCategoryRelated = [];
-    const as = [];
+    const productsFilterByCategory = [];
     const productsSubLevelByCategory = products.filter((product) => {
         product.subCategoriesId.map((subLevel) => {
             if(subLevel == subLevelsState){
-                as.push(product)
+                productsFilterByCategory.push(product)
                 return product;
             }
         }) 
@@ -81,11 +82,9 @@ export default function Slug({slug}){
             }
             return {active: false, value: prev.value}
         })
-
-        console.log("estas apretando")
     }
 
-    console.log("cambiaa", orderFilterState)
+    console.log(filterSelected)
 
     const [categoriesFilterState, setCategoriesFilterState] = useState(false);
 
@@ -113,7 +112,7 @@ export default function Slug({slug}){
 
                         <div className="w-[45%] shadow-default bg-green-500 h-auto flex justify-center items-center p-2 py-3 rounded-lg relative">
                             <FontAwesomeIcon className="text-white w-[24px] h-[24px]" icon={faArrowDownWideShort} />
-                            <p onClick={() => setSubCategoriesState(state => !state)} className="ml-2 tracking-wide text-white font-medium">CATEGORIAS</p>
+                            <p onClick={() => setFilterProductState(state => !state)} className="ml-2 tracking-wide text-white font-medium">CATEGORIAS</p>
                             
                         </div>
                     </div>
@@ -121,11 +120,11 @@ export default function Slug({slug}){
                 </div>
 
                 <section className="w-full h-full">
-                    <div className="w-full h-auto flex justify-between items-center">
-                        <div className="w-auto flex flex-row items-center">
+                    <div className="w-full h-auto flex justify-between items-center mb-10">
+                        <div className="w-auto flex flex-row items-center relative">
                             <p className="text-black">Ordenar por:</p>
                             <div
-                                className="w-auto flex flex-row items-center relative">
+                                className="w-auto flex flex-row items-center">
                                 <p onClick={handleActiveOrder} className="ml-2 text-black font-medium">{orderFilterState.value}</p>
                                 <FontAwesomeIcon onClick={handleActiveOrder} className="w-[16px] h-[16px] ml-2 text-green-500" icon={faAngleDown} />
                                 <OrderProducts 
@@ -137,81 +136,97 @@ export default function Slug({slug}){
                         <div className="w-auto flex flex-row items-center">
                             <div
                                 className="w-auto flex flex-row items-center relative bg-green-500 p-2 rounded-md">
-                                <p onClick={() => setSubCategoriesState(state => !state)} className="ml-2 text-white font-medium">Filtrar</p>
-                                <FontAwesomeIcon onClick={() => setSubCategoriesState(state => !state)} className="w-[16px] h-[16px] ml-2 text-white rotate-[270deg]" icon={faAngleDown} />
-                                <SubCategories 
-                                    subCategoriesState={subCategoriesState}
+                                <p onClick={() => setFilterProductState(state => !state)} className="ml-2 text-white font-medium">Filtrar</p>
+                                <FontAwesomeIcon onClick={() => setFilterProductState(state => !state)} className="w-[16px] h-[16px] ml-2 text-white rotate-[270deg]" icon={faAngleDown} />
+                                <Filter 
+                                    filterProductState={filterProductState}
                                     categories={categorySelected[0].subLevels}
                                     handleSubLevelSelect={handleSubLevelSelect}
-                                    setSubCategoriesState={setSubCategoriesState}
+                                    subLevelsState={subLevelsState}
+                                    setFilterProductState={setFilterProductState}
+                                    setFilterSelected={setFilterSelected}
                                 />
                             </div>
                         </div>
                     </div>
-                    <ul className="w-full h-full flex justify-evenly flex-wrap py-4">
-                        {subLevelsState == 0 ? productsCategoryRelated.map((product) => (
-                            <li className="relative w-[150px] h-[280px] rounded-md mb-8 mr-2 flex flex-col justify-evenly items-center" key={product.id}>
-                                <div className="animate-subtitleAppear w-full h-full bg-white rounded-md shadow-default flex flex-col items-center">
-                                    <Link className="w-full h-[140px] mb-2" href={`/products/${product.id}`}>
-                                        <figure className="mb-2 w-full h-[140px]">
-                                            <Image className="object-cover rounded-t-md object-center w-full h-full"  priority={true} width={140} height={140} src={product.src} alt={product.name} />
-                                        </figure>
-                                    </Link>
-                                    <div className="w-full h-full flex justify-around items-center flex-col">
-                                        <div className="w-full h-[32px] text-left pl-2 mb-2">
-                                            <h4 className="text-green-700 text-xs font-bold">
-                                                {product.name.length > 40 ? `${product.name.slice(0, 40)}...` : product.name}
-                                            </h4>
-                                        </div>
-                                        <div className="w-full text-left pl-2 mb-2">
-                                            <p className="text-green-700 text-base font-bold tracking-wide mr-2">
-                                                {`$${product.price}`}
-                                            </p>
-                                        </div>
-                                        <div className="w-full px-2 h-auto flex justify-start mb-2">
-                                            <button className="bg-green-400 text-white w-full rounded-md tracking-wider p-1 font-bold flex justify-center items-center" type="text">
-                                                <FontAwesomeIcon className="w-[20px] h-[20px]" icon={faCartShopping} />
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        ))
-                        
-                        :
 
-                        <>
-                        {as.map((product) => (
-                            <li className="relative w-[150px] h-[280px] rounded-md mb-8 mr-2 flex flex-col justify-evenly items-center" key={product.id}>
-                                <div className="animate-subtitleAppear w-full h-full bg-white rounded-md shadow-default flex flex-col items-center">
-                                    <Link className="w-full h-[140px] mb-2" href={`/products/${product.id}`}>
-                                        <figure className="mb-2 w-full h-[140px]">
-                                            <Image className="object-cover rounded-t-md object-center w-full h-full"  priority={true} width={140} height={140} src={product.src} alt={product.name} />
-                                        </figure>
-                                    </Link>
-                                    <div className="w-full h-full flex justify-around items-center flex-col">
-                                        <div className="w-full h-[32px] text-left pl-2 mb-2">
-                                            <h4 className="text-green-700 text-xs font-bold">
-                                                {product.name.length > 40 ? `${product.name.slice(0, 40)}...` : product.name}
-                                            </h4>
-                                        </div>
-                                        <div className="w-full text-left pl-2 mb-2">
-                                            <p className="text-green-700 text-base font-bold tracking-wide mr-2">
-                                                {`$${product.price}`}
-                                            </p>
-                                        </div>
-                                        <div className="w-full px-2 h-auto flex justify-start mb-2">
-                                            <button className="bg-green-400 text-white w-full rounded-md tracking-wider p-1 font-bold flex justify-center items-center" type="text">
-                                                <FontAwesomeIcon className="w-[20px] h-[20px]" icon={faCartShopping} />
-                                            </button>
+                    <article className="w-full h-full">
+                        {!filterSelected ? 
+                            <></>
+
+                            :
+
+                            <ul>
+                                {filterSelected.map((item) => (
+                                    <li>{item}</li>
+                                ))}
+                            </ul>
+                        }
+                        <ul className="w-full h-full flex justify-evenly flex-wrap py-4">
+                            {subLevelsState == 0 ? productsCategoryRelated.map((product) => (
+                                <li className="relative w-[150px] h-[280px] rounded-md mb-8 mr-2 flex flex-col justify-evenly items-center" key={product.id}>
+                                    <div className="animate-subtitleAppear w-full h-full bg-white rounded-md shadow-default flex flex-col items-center">
+                                        <Link className="w-full h-[140px] mb-2" href={`/products/${product.id}`}>
+                                            <figure className="mb-2 w-full h-[140px]">
+                                                <Image className="object-cover rounded-t-md object-center w-full h-full"  priority={true} width={140} height={140} src={product.src} alt={product.name} />
+                                            </figure>
+                                        </Link>
+                                        <div className="w-full h-full flex justify-around items-center flex-col">
+                                            <div className="w-full h-[32px] text-left pl-2 mb-2">
+                                                <h4 className="text-green-700 text-xs font-bold">
+                                                    {product.name.length > 40 ? `${product.name.slice(0, 40)}...` : product.name}
+                                                </h4>
+                                            </div>
+                                            <div className="w-full text-left pl-2 mb-2">
+                                                <p className="text-green-700 text-base font-bold tracking-wide mr-2">
+                                                    {`$${product.price}`}
+                                                </p>
+                                            </div>
+                                            <div className="w-full px-2 h-auto flex justify-start mb-2">
+                                                <button className="bg-green-400 text-white w-full rounded-md tracking-wider p-1 font-bold flex justify-center items-center" type="text">
+                                                    <FontAwesomeIcon className="w-[20px] h-[20px]" icon={faCartShopping} />
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </li>
-                        ))}
-                        </>
-                    }
-                    </ul>
+                                </li>
+                            ))
+                            
+                            :
+
+                            <>
+                            {productsFilterByCategory.map((product) => (
+                                <li className="relative w-[150px] h-[280px] rounded-md mb-8 mr-2 flex flex-col justify-evenly items-center" key={product.id}>
+                                    <div className="animate-subtitleAppear w-full h-full bg-white rounded-md shadow-default flex flex-col items-center">
+                                        <Link className="w-full h-[140px] mb-2" href={`/products/${product.id}`}>
+                                            <figure className="mb-2 w-full h-[140px]">
+                                                <Image className="object-cover rounded-t-md object-center w-full h-full"  priority={true} width={140} height={140} src={product.src} alt={product.name} />
+                                            </figure>
+                                        </Link>
+                                        <div className="w-full h-full flex justify-around items-center flex-col">
+                                            <div className="w-full h-[32px] text-left pl-2 mb-2">
+                                                <h4 className="text-green-700 text-xs font-bold">
+                                                    {product.name.length > 40 ? `${product.name.slice(0, 40)}...` : product.name}
+                                                </h4>
+                                            </div>
+                                            <div className="w-full text-left pl-2 mb-2">
+                                                <p className="text-green-700 text-base font-bold tracking-wide mr-2">
+                                                    {`$${product.price}`}
+                                                </p>
+                                            </div>
+                                            <div className="w-full px-2 h-auto flex justify-start mb-2">
+                                                <button className="bg-green-400 text-white w-full rounded-md tracking-wider p-1 font-bold flex justify-center items-center" type="text">
+                                                    <FontAwesomeIcon className="w-[20px] h-[20px]" icon={faCartShopping} />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            ))}
+                            </>
+                        }
+                        </ul>
+                    </article>
                 </section>
 
                 <CategoriesFilter
