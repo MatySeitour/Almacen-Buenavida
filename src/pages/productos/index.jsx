a15eab8322bf6ca688c04dc8e66d35edb4765258
Matias Seitour <matias.seitour01@gmail.com>
2023-05-05 11:31:42 -0300

    feat(productos/index) import ProductsLayout and ProductsIndex components

--- src/pages/productos/index.jsx
+++ src/pages/productos/index.jsx
@@ -3,15 +3,20 @@ import Link from "next/link";
 import getProducts from "../../utils/products.json"
 import getCategories from "../../utils/categories.json"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping, faFilter, faArrowDownWideShort } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faAngleDown, faAngleRight, faArrowDownWideShort } from "@fortawesome/free-solid-svg-icons";
 import CartHome from "@/components/CartHome";
 import CategoriesFilter from "@/components/CategoriesFilter";
 import { useState } from "react";
 import { useCart } from "@/context/CartContext";
-import Layout from "@/components/Layout";
+import { useCategory } from "@/context/CategoriesContext";
+import OrderProducts from "@/components/OrderProducts";
+import ProductsLayout from "./layout";
+import ProductsIndex from "@/components/ProductsIndex";
+
 
 export default function Productos() {
     const {cartShow} = useCart();
+    const {orderFilterState, setOrderFilterState, handleActiveOrder} = useCategory();
 
     const products = getProducts.products;
     const categories = getCategories.categories;
@@ -20,31 +25,30 @@ export default function Productos() {
 
 
     return (
-        <Layout>
-            <section className="w-full h-auto min-h-screen pt-36 bg-slate-100">
-                <div className="w-full h-auto p-2 mb-4">
-                    <div className="w-[full] h-auto flex flex-row justify-center items-center px-2 mb-4">
-                        <p className="w-[70%] text-center text-base text-green-500">{`Inicio > `} <b>Productos</b></p>
-                    </div>
-                    <div className="w-full h-auto p-2 border-t border-green-500 pt-4">
-                        <h2 className="text-center text-3xl text-black font-semibold">Productos</h2>
-                    </div>
+        <ProductsLayout>
+            <>
+                <ProductsIndex />
+                <div className="w-full h-auto flex justify-between items-center mb-10">
+                        <div className="w-auto flex flex-row items-center relative">
+                            <p className="text-black">Ordenar por:</p>
+                            <div className="w-auto flex flex-row items-center">
+                                <p onClick={handleActiveOrder} className="ml-2 text-black font-medium">{orderFilterState.value}</p>
+                                <FontAwesomeIcon onClick={handleActiveOrder} className="w-[16px] h-[16px] ml-2 text-green-500" icon={faAngleDown} />
+                                <OrderProducts 
+                                    orderFilterState={orderFilterState}
+                                    setOrderFilterState={setOrderFilterState}
+                                />
+                            </div>
+                        </div>
+                    <button onClick={() => setCategoriesFilterState(true)} className="w-auto flex flex-row items-center relative bg-green-500 p-2 rounded-md">
+                        <p className="mr-2 text-white text-sm font-medium tracking-wider">FILTRAR</p>
+                        <FontAwesomeIcon className="text-white w-[14px] h-[14px]" icon={faAngleRight} />
+                    </button>
                 </div>
 
-                <div className="w-full h-auto flex flex-row justify-between p-2">
-                    <button type="text" className="w-[45%] h-auto flex shadow-default bg-green-500 justify-center items-center p-3 rounded-lg border-2 border-green-500">
-                        <FontAwesomeIcon className="text-white w-[24px] h-[24px]" icon={faFilter} />
-                        <p className="ml-3 tracking-wide text-white">ORDENAR</p>
-                    </button>
-                    <button onClick={() => setCategoriesFilterState(true)} className="w-[45%] shadow-default bg-green-500 h-auto flex justify-center items-center p-3 rounded-lg border-2 border-green-500">
-                        <FontAwesomeIcon className="text-white w-[24px] h-[24px]" icon={faArrowDownWideShort} />
-                        <p className="ml-3 tracking-wide text-white">CATEGORIAS</p>
-                    </button>
-                </div>
-
-                <ul className="w-full h-full flex justify-evenly flex-wrap py-8">
+                <ul className="w-full h-full flex justify-around flex-wrap py-8">
                     {products.map((product) => (
-                        <li className="relative w-[150px] h-[280px] rounded-md mb-8 mr-2 flex flex-col justify-evenly items-center" key={product.id}>
+                        <li className="relative w-[150px] h-[280px] rounded-md mb-8 flex flex-col justify-evenly items-center" key={product.id}>
                         <div className="animate-subtitleAppear w-full h-full bg-white rounded-md shadow-default flex flex-col items-center">
                             <Link className="w-full h-[140px] mb-2" href={`/products/${product.id}`}>
                                 <figure className="mb-2 w-full h-[140px]">
@@ -81,7 +85,7 @@ export default function Productos() {
                 <CartHome
                     cartShow={cartShow}
                 />
-            </section>  
-        </Layout>
+            </>  
+        </ProductsLayout>
     )
 }
