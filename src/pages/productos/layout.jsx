9f460153bdd3923f704ab6b999124e0194f5ea13
Matias Seitour <matias.seitour01@gmail.com>
2023-05-05 11:27:02 -0300

    feat(productsLayout) add products layout

---
+++ src/pages/productos/layout.jsx
@@ -0,0 +1,19 @@
+import ContactSection from "@/components/ContactSection";
+import Nav from "@/components/Nav";
+
+export default function ProductsLayout({children}){
+    return(
+        <>
+            <Head>
+                <title>Almacen Buenavida - Productos</title>
+                <meta name='description' content='Pagina web de almacen buenavida'></meta>
+            </Head>
+
+            <Nav />
+            <section className="w-full h-auto min-h-screen pt-4 bg-slate-100 px-4">
+                {children}
+            </section>
+            <ContactSection />
+        </>
+    )
+}
