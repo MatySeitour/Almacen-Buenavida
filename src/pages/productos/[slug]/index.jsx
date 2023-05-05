274054cbf4f6792cebadf3e37f0d669d4d3bd18a
Matias Seitour <matias.seitour01@gmail.com>
2023-05-05 11:32:19 -0300

    feat(productos/slug/index) import ProductsLayout and ProductsIndex components

--- src/pages/productos/[slug]/index.jsx
+++ src/pages/productos/[slug]/index.jsx
@@ -8,10 +8,11 @@ import CartHome from "@/components/CartHome";
 import CategoriesFilter from "@/components/CategoriesFilter";
 import { useState } from "react";
 import { useCart } from "@/context/CartContext";
-import Layout from "@/components/Layout";
+import ProductsLayout from "../layout";
 import { useCategory } from "@/context/CategoriesContext";
 import Filter from "@/components/Filter";
 import OrderProducts from "@/components/OrderProducts";
+import ProductsIndex from "@/components/ProductsIndex";
 
 export async function getServerSideProps(context){
     const {params} = context;
@@ -25,19 +26,17 @@ export async function getServerSideProps(context){
 
 export default function Slug({slug}){
     const {cartShow} = useCart();
-
-    const {setFilterProductState, filterProductState} = useCategory();
+    const {orderFilterState, setOrderFilterState, handleActiveOrder, setFilterProductState, filterProductState, filterSelected ,setFilterSelected} = useCategory();
 
     const [subLevelsState, setSubLevelsState] = useState(0);
-    const [orderFilterState, setOrderFilterState] = useState({
-        active: false,
-        value: "A - Z"
-    });
+    // const [orderFilterState, setOrderFilterState] = useState({
+    //     active: false,
+    //     value: "A - Z"
+    // });
 
     
     const categories = getCategories.categories;
     const categorySelected = categories.filter(category => category.slug == slug);
-    const [filterSelected, setFilterSelected] = useState([]);
 
     const products = getProducts.products;
     const productsByCategory = products.filter(product => product.categoryId == categorySelected[0].id);
@@ -75,50 +74,25 @@ export default function Slug({slug}){
         }
     }
 
-    const handleActiveOrder = () => {
-        setOrderFilterState((prev) => {
-            if(!prev.active){
-                return {active: true, value: prev.value}
-            }
-            return {active: false, value: prev.value}
-        })
-    }
+    // const handleActiveOrder = () => {
+    //     setOrderFilterState((prev) => {
+    //         if(!prev.active){
+    //             return {active: true, value: prev.value}
+    //         }
+    //         return {active: false, value: prev.value}
+    //     })
+    // }
 
     console.log(filterSelected)
 
     const [categoriesFilterState, setCategoriesFilterState] = useState(false);
 
     return(
-        <Layout>
-            <section className="w-full h-auto min-h-screen pt-4 bg-slate-100 px-4">
-                <div className="w-full h-auto p-2 mb-4">
-                    <div className="w-[full] h-auto flex flex-row justify-center items-center px-2 pb-2 mb-4 border-b border-green-500">
-                        <p className="w-[70%] text-center text-base text-green-500">{`Inicio > `} Productos{` > `} <b>{categorySelected[0].name}</b></p>
-                    </div>
-                    <div className="w-full h-auto p-2">
-                        <h2 className="text-center text-3xl font-semibold text-green-500">{categorySelected[0].name}</h2>
-                    </div>
-                </div>
-
-                <div className="w-full h-auto py-4">
-                    <div className="w-full h-auto flex flex-row justify-between p-2 mb-10 relative">
-                        {/* <div onClick={() => setOrderFilterState(state => !state)} type="text" className="w-[45%] shadow-default h-auto flex bg-green-500 justify-center items-center p-2 py-3 rounded-lg relative">
-                            <FontAwesomeIcon className="text-white w-[24px] h-[24px]" icon={faFilter} />
-                            <p className="ml-3 tracking-wide text-white font-medium">ORDENAR</p>
-                            <OrderProducts 
-                                orderFilterState={orderFilterState}
-                            />
-                        </div> */}
-
-                        <div className="w-[45%] shadow-default bg-green-500 h-auto flex justify-center items-center p-2 py-3 rounded-lg relative">
-                            <FontAwesomeIcon className="text-white w-[24px] h-[24px]" icon={faArrowDownWideShort} />
-                            <p onClick={() => setFilterProductState(state => !state)} className="ml-2 tracking-wide text-white font-medium">CATEGORIAS</p>
-                            
-                        </div>
-                    </div>
-                    
-                </div>
-
+        <ProductsLayout>
+            <>
+                <ProductsIndex 
+                    slug={slug}
+                />
                 <section className="w-full h-full">
                     <div className="w-full h-auto flex justify-between items-center mb-10">
                         <div className="w-auto flex flex-row items-center relative">
@@ -134,8 +108,7 @@ export default function Slug({slug}){
                             </div>
                         </div>
                         <div className="w-auto flex flex-row items-center">
-                            <div
-                                className="w-auto flex flex-row items-center relative bg-green-500 p-2 rounded-md">
+                            <div className="w-auto flex flex-row items-center relative bg-green-500 p-2 rounded-md">
                                 <p onClick={() => setFilterProductState(state => !state)} className="ml-2 text-white font-medium">Filtrar</p>
                                 <FontAwesomeIcon onClick={() => setFilterProductState(state => !state)} className="w-[16px] h-[16px] ml-2 text-white rotate-[270deg]" icon={faAngleDown} />
                                 <Filter 
@@ -145,6 +118,7 @@ export default function Slug({slug}){
                                     subLevelsState={subLevelsState}
                                     setFilterProductState={setFilterProductState}
                                     setFilterSelected={setFilterSelected}
+                                    slugCategory={slug}
                                 />
                             </div>
                         </div>
@@ -163,7 +137,7 @@ export default function Slug({slug}){
                             </ul>
                         }
                         <ul className="w-full h-full flex justify-evenly flex-wrap py-4">
-                            {subLevelsState == 0 ? productsCategoryRelated.map((product) => (
+                            {productsCategoryRelated.map((product) => (
                                 <li className="relative w-[150px] h-[280px] rounded-md mb-8 mr-2 flex flex-col justify-evenly items-center" key={product.id}>
                                     <div className="animate-subtitleAppear w-full h-full bg-white rounded-md shadow-default flex flex-col items-center">
                                         <Link className="w-full h-[140px] mb-2" href={`/products/${product.id}`}>
@@ -191,39 +165,6 @@ export default function Slug({slug}){
                                     </div>
                                 </li>
                             ))
-                            
-                            :
-
-                            <>
-                            {productsFilterByCategory.map((product) => (
-                                <li className="relative w-[150px] h-[280px] rounded-md mb-8 mr-2 flex flex-col justify-evenly items-center" key={product.id}>
-                                    <div className="animate-subtitleAppear w-full h-full bg-white rounded-md shadow-default flex flex-col items-center">
-                                        <Link className="w-full h-[140px] mb-2" href={`/products/${product.id}`}>
-                                            <figure className="mb-2 w-full h-[140px]">
-                                                <Image className="object-cover rounded-t-md object-center w-full h-full"  priority={true} width={140} height={140} src={product.src} alt={product.name} />
-                                            </figure>
-                                        </Link>
-                                        <div className="w-full h-full flex justify-around items-center flex-col">
-                                            <div className="w-full h-[32px] text-left pl-2 mb-2">
-                                                <h4 className="text-green-700 text-xs font-bold">
-                                                    {product.name.length > 40 ? `${product.name.slice(0, 40)}...` : product.name}
-                                                </h4>
-                                            </div>
-                                            <div className="w-full text-left pl-2 mb-2">
-                                                <p className="text-green-700 text-base font-bold tracking-wide mr-2">
-                                                    {`$${product.price}`}
-                                                </p>
-                                            </div>
-                                            <div className="w-full px-2 h-auto flex justify-start mb-2">
-                                                <button className="bg-green-400 text-white w-full rounded-md tracking-wider p-1 font-bold flex justify-center items-center" type="text">
-                                                    <FontAwesomeIcon className="w-[20px] h-[20px]" icon={faCartShopping} />
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            ))}
-                            </>
                         }
                         </ul>
                     </article>
@@ -237,7 +178,7 @@ export default function Slug({slug}){
                 <CartHome
                     cartShow={cartShow}
                 />
-            </section>
-        </Layout>
+            </>
+        </ProductsLayout>
     )
 }
