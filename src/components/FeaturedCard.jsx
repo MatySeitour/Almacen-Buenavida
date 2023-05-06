c52a5f62cabb0c2a9d0ec206e62dfc0834c5e6a4
Matias Seitour <matias.seitour01@gmail.com>
2023-05-06 12:54:54 -0300

    feat(featuredCard) import buttonQuantityCard && productPriceCard

--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -2,28 +2,43 @@ import Image from "next/image";
 import Link from "next/link";
 import useIntersection from "@/CustomHooks/useIntersection";
 import { useRef } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import ProductPriceCard from "./ProductPriceCard";
+import ButtonQuantityCard from "./ButtonQuantityCard";
 
-export default function FeaturedCard({ productSrc, productName, productStock, productPrice, productId }) {
+export default function FeaturedCard({ productSrc, productName, productStock, productPrice, productId, productOffer }) {
     const element = useRef(null);
     const screen = useIntersection(element);
+
     return (
         <li ref={element} className="relative w-[150px] h-[280px] rounded-md mb-8 mr-2 flex flex-col justify-evenly items-center">
             {screen ?
-                <Link href={`/products/${productId}`} className="animate-subtitleAppear w-full h-full bg-white rounded-md shadow-default flex flex-col items-center">
+                <div className="animate-subtitleAppear w-full h-full bg-white rounded-md shadow-default flex flex-col items-center">
                     {!productStock ? <p className="absolute p-2 text-sm bg-green-500 left-2 top-2 font-bold text-white rounded-md">SIN STOCK</p> : <></>}
-                    <figure className="mb-2 w-full h-[140px]">
+                    <span className="absolute w-[40px] h-[40px] rounded-full bg-green-500 -top-4 -right-4 flex text-center justify-center z-[20]">
+                        <p className="flex justify-center items-center text-white font-bold text-xs">{productOffer == "2x1" ? "2x1" : `-${productOffer}%`}</p>
+                    </span>
+                    <Link href={`/products/${productId}`} className="mb-2 w-full h-[140px]">
+                    <figure className="w-full h-[100%] min-h-[140px]">
                         <Image className="object-cover rounded-t-md object-center w-full h-full" priority={true} width={140} height={140} alt={productName} src={productSrc} />
                     </figure>
+                    </Link>
                     <div className="w-full h-full flex justify-around items-center flex-col">
                         <div className="w-full h-[32px] text-left pl-2 mb-2"><h4 className="text-green-700 text-xs font-bold">{productName.length > 40 ? `${productName.slice(0, 40)}...` : productName}</h4></div>
-                        <div className="w-full pl-2 mb-2"><p className="text-green-700 text-lg font-bold tracking-wide mr-2 text-right">{`$${productPrice}`}</p></div>
-                        <div className="w-full px-2 h-auto flex justify-start mb-2">
-                            <button className="bg-green-400 text-white w-full rounded-md tracking-wider p-1 font-bold flex justify-center items-center" type="text"><FontAwesomeIcon className="w-[20px] h-[20px]" icon={faCartShopping} /></button>
+                            <ProductPriceCard 
+                                productOffer={productOffer}
+                                productPrice={productPrice}
+                            />
+                        <div className="w-full px-2 h-auto min-h-[34px] flex justify-center mb-2">
+                            <ButtonQuantityCard 
+                                productSrc={productSrc}
+                                productId={productId}
+                                productOffer={productOffer}
+                                productName={productName}
+                                productPrice={productPrice}
+                            /> 
                         </div>
                     </div>
-                </Link>
+                </div>
 
                 :
 
