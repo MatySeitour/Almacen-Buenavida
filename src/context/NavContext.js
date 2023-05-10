db72fbc08f1ea6e143fb5f803a9bcc7bd4c2a70a
Matias Seitour <matias.seitour01@gmail.com>
2023-05-10 11:01:04 -0300

    feat(navContext) add navItems

--- src/context/NavContext.js
+++ src/context/NavContext.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { createContext, useContext, useState, useEffect } from "react";
+import { faStore, faPlateWheat, faListUl, faUsers, faBagShopping } from "@fortawesome/free-solid-svg-icons";
 
 const NavContext = createContext({});
 
@@ -13,8 +14,37 @@ export const useNav = () => {
 }
 
 export function NavContextProvider({ children }) {
+    const [navItemsState, setNavItemsState] = useState([
+        {
+            id: 1,
+            navItem: "Home",
+            icon: faStore,
+        },
+        {
+            id: 2,
+            navItem: "Productos",
+            icon: faPlateWheat
+        },
+        {
+            id: 3,
+            navItem: "Categorias",
+            active: false,
+            icon: faListUl
+        },
+        {
+            id: 4,
+            navItem: "Sobre Nosotros",
+            icon: faUsers
+        },
+        {
+            id: 5,
+            navItem: "Como Comprar",
+            icon: faBagShopping
+        }
+    ])
+
     return (
-        <NavContext.Provider value={{}}>
+        <NavContext.Provider value={{ useNav, navItemsState, setNavItemsState }}>
             {children}
         </NavContext.Provider>
     )
